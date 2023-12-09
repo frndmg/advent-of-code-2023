b2524f58822fc4ff864c41f09d9eeb570c667062
Fernando Martínez González <frndmartinezglez@gmail.com>
2023-12-09 17:24:22 +0100

    Solve day 9 in Haskell

---
+++ 09/Main.hs
@@ -0,0 +1,43 @@
+import Text.Parsec (parse, sepEndBy, sepBy1, eof)
+import Text.Parsec.String (Parser)
+import Text.Parsec.Char (newline, string, digit, char)
+import Text.Parsec.Combinator (many1)
+import Control.Applicative ((<|>))
+
+test :: String
+test = "0 3 6 9 12 15\n\
+       \1 3 6 10 15 21\n\
+       \10 13 16 21 30 45"
+
+diff :: [Int] -> [Int]
+diff xs = map (uncurry (-)) $ zip (tail xs) (init xs)
+
+predict :: [Int] -> Int
+predict xs = sum $ map last $ takeWhile (any (/= 0)) $ iterate diff xs
+
+extrapolate :: [Int] -> Int
+extrapolate xs = foldl (\acc x -> x - acc) 0 $ reverse $ map head $ takeWhile (any (/= 0)) $ iterate diff xs
+
+number :: Parser Int
+number = read <$> (many1 $ digit <|> (char '-'))
+
+spaces = many1 $ string " "
+
+parseNumberList :: Parser [Int]
+parseNumberList = number `sepBy1` spaces
+
+parsePuzzle :: Parser [[Int]]
+parsePuzzle = parseNumberList `sepEndBy` newline <* eof
+
+makePuzzle :: String -> [[Int]]
+makePuzzle text = let Right xss = parse parsePuzzle "" text in xss
+
+main1 :: IO ()
+main1 = do
+  content <- readFile "input.txt"
+  print $ sum $ map predict $ makePuzzle content
+
+main2 :: IO ()
+main2 = do
+  content <- readFile "input.txt"
+  print $ sum $ map extrapolate $ makePuzzle content
