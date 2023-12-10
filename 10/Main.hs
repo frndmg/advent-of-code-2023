94c16f5349cfd96f0339463476d9948791334e2a
Fernando Martínez González <frndmartinezglez@gmail.com>
2023-12-10 17:23:38 +0100

    Solve 2nd part

--- 10/Main.hs
+++ 10/Main.hs
@@ -3,7 +3,7 @@ import qualified Data.Set as S
 import Data.Map (Map)
 import Data.Set (Set)
 
-import Data.Maybe (isJust, fromJust)
+import Data.Maybe (isJust, fromJust, catMaybes)
 
 import Data.List (sort)
 
@@ -34,6 +34,37 @@ test2 = "7-F7-\n\
         \|F--J\n\
         \LJ.LJ"
 
+test3 = "...........\n\
+        \.S-------7.\n\
+        \.|F-----7|.\n\
+        \.||.....||.\n\
+        \.||.....||.\n\
+        \.|L-7.F-J|.\n\
+        \.|..|.|..|.\n\
+        \.L--J.L--J.\n\
+        \..........."
+
+test4 = "..........\n\
+        \.S------7.\n\
+        \.|F----7|.\n\
+        \.||....||.\n\
+        \.||....||.\n\
+        \.|L-7F-J|.\n\
+        \.|..||..|.\n\
+        \.L--JL--J.\n\
+        \.........."
+
+test5 = ".F----7F7F7F7F-7....\n\
+        \.|F--7||||||||FJ....\n\
+        \.||.FJ||||||||L7....\n\
+        \FJL7L7LJLJ||LJ.L-7..\n\
+        \L--J.L7...LJS7F-7L7.\n\
+        \....F-J..F7FJ|L7L7L7\n\
+        \....L7.F7||L7|.L7L7|\n\
+        \.....|FJLJ|FJ|F7|.LJ\n\
+        \....FJL-7.||.||||...\n\
+        \....L---J.LJ.LJLJ..."
+
 makeGraph :: String -> Graph
 makeGraph text = M.fromList
   [ ((row, column), pipeFromChar x)
@@ -48,6 +79,59 @@ makeLoop g initPos = go (S.fromList [initPos]) [initPos] (connectedNeighbors g i
     go visited nodes (x:xs)
       | S.member x visited = go visited nodes xs
       | otherwise = go (S.insert x visited) (x:nodes) (connectedNeighbors g x)
+
+pointsInsideLoop :: Graph -> [Pos] -> [Pos]
+pointsInsideLoop g loop = filter (insideLoop g' loopSet') candidates
+  where loopSet = S.fromList loop
+        g' = updateInit g (last loop)
+        loopSet' = S.fromList $ filter (\x -> notVPipe $ M.lookup x g) $ loop
+        candidates = filter (\x -> not $ S.member x loopSet) $ M.keys g
+
+updateInit :: Graph -> Pos -> Graph
+updateInit g pos = M.update go pos g
+  where
+    [a, b] = connectedNeighbors g pos
+    go Init = Just $ case (dirRelative pos a, dirRelative pos b) of
+      (N, E) -> NEPipe
+      (E, N) -> NEPipe
+      (N, W) -> NWPipe
+      (W, N) -> NWPipe
+      (S, E) -> SEPipe
+      (E, S) -> SEPipe
+      (S, W) -> SWPipe
+      (W, S) -> SWPipe
+      (N, S) -> VPipe
+      (S, N) -> VPipe
+      (E, W) -> HPipe
+      (W, E) -> HPipe
+    go _ = Nothing
+
+data Dir = N | S | E | W
+
+dirRelative :: Pos -> Pos -> Dir
+dirRelative a@(ax, ay) b@(bx, by)
+  | ax < bx && ay == by = S
+  | bx < ax && ay == by= N
+  | ay < by && ax == bx = E
+  | by < ay && ax == bx = W
+  | otherwise = error "What?!"
+
+notVPipe p = case p of
+  Just VPipe -> False
+  _ -> True
+
+countNots :: [Pipe] -> Int
+countNots [] = 0
+countNots (SEPipe:NWPipe:rest) = 1 + countNots rest
+countNots (SWPipe:NEPipe:rest) = 1 + countNots rest
+countNots (_:rest) = 1 + countNots rest
+
+insideLoop :: Graph -> Set Pos -> Pos -> Bool
+insideLoop g loop (x, y) =
+  (== 1) $ (`mod` 2) $ countNots
+  $ catMaybes $ map (\x -> M.lookup x g)
+  $ filter (\p -> S.member p loop)
+  $ [(z, y) | z <- [1..x]]
 
 connected :: Graph -> Pos -> Pos -> Bool
 connected g x y
@@ -110,3 +194,11 @@ main1 = do
   let g = makeGraph content
   let initPos = (!! 0) $ map fst $ filter (\(_, x) -> x == Init) $ M.toList g
   print $ (`div` 2) $ length $ makeLoop g initPos
+
+main2 :: IO ()
+main2 = do
+  content <- readFile "input.txt"
+  let g = makeGraph content
+  let initPos = (!! 0) $ map fst $ filter (\(_, x) -> x == Init) $ M.toList g
+  let loop = makeLoop g initPos
+  print $ length $ pointsInsideLoop g loop
